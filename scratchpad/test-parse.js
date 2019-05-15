8577ab6c6630e5b63d09928149d645c97a6b80e8
Kevin Lindsey <kevin@kevlindev.com>
2019-05-15 15:11:29 -0700

    Start working on a jison-based parser
    This isn’t the fastest parser, but I’m familiar with it so I can make progress quickly. During this prototyping phase, I need something that can allow me to change grammars quickly. This does the trick. Once the grammar has settled, I may consider another parsing engine.

--- scratchpad/test-parse.js
+++ scratchpad/test-parse.js
@@ -1,17 +1,45 @@
 #!/usr/bin/env node -r esm
 
 import fs from "fs";
-import ValidatorParser from "../lib/ValidatorParser.js";
+import util from "util";
+import Parser from "../lib/Parser.js";
 
-function testValidation(structure) {
-    const result = validator.validate(structure, "Ellipse");
-    const status = result ? "  valid" : "invalid";
+function testParser(source) {
+    const result = Parser.parse(source);
+    const prettyPrint = util.inspect(result, { depth: Infinity });
 
-    console.log(`${status}: ${JSON.stringify(structure)}`);
+    console.log(source);
+    console.log(prettyPrint);
+    console.log();
 }
 
-const source = fs.readFileSync("./test.normalizer", "utf-8");
-const parser = new ValidatorParser();
-const validatorTable = parser.parse(source);
+const testValidations = [
+    'type Ellipse = true',
+    'type Ellipse = false',
+    'type Ellipse = boolean',
+    'type Ellipse = "hello"',
+    'type Ellipse = string',
+    'type Ellipse = 10',
+    'type Ellipse = number',
+    'type Ellipse = {}',
+    'type Ellipse = []',
+    'type Ellipse = { cx }',
+    'type Ellipse = { cx cy }',
+    'type Ellipse = { cx { group { match cx { boolean } } } }',
+    'type Ellipse = { cx { group { match cx { true } } } }',
+    'type Ellipse = { cx { group { match cx { false } } } }',
+    'type Ellipse = { cx { group { match cx { 10 } } } }',
+    'type Ellipse = { cx { group { match cx { string } } } }',
+    'type Ellipse = { cx { group { match cx { "first name" } } } }',
+    'type Ellipse = { cx { group { match cx { string number } } } }',
+    'type Ellipse = { cx { group { match cx { number } match cy { number } } } }',
+    // initial with matching shortcuts. I use cy in match to make it clear that can be changed
+    'type Ellipse = { cx { group { match cy { number } } } }',
+    'type Ellipse = { cx { group { cy: number } } }',
+    'type Ellipse = { cx { cy: number } }',
+    'type Ellipse = { cx: number }'
+];
 
-console.log(validatorTable);
+for (const source of testValidations) {
+    testParser(source);
+}
