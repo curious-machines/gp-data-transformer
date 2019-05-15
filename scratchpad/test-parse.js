0fc98d81555eb78da42274704dfcd0193123d62d
Kevin Lindsey <kevin@kevlindev.com>
2019-05-15 16:14:45 -0700

    Add support for array descriptions
    This also adds return value expressions to canonical properties, although we’re not parsing the body of those particular constructs yet

--- scratchpad/test-parse.js
+++ scratchpad/test-parse.js
@@ -5,12 +5,18 @@ import util from "util";
 import Parser from "../lib/Parser.js";
 
 function testParser(source) {
-    const result = Parser.parse(source);
-    const prettyPrint = util.inspect(result, { depth: Infinity });
+    try {
+        const result = Parser.parse(source);
+        const prettyPrint = util.inspect(result, { depth: Infinity });
 
-    console.log(source);
-    console.log(prettyPrint);
-    console.log();
+        console.log(source);
+        console.log(prettyPrint);
+        console.log();
+    }
+    catch(e) {
+        console.log(source);
+        console.log(e);
+    }
 }
 
 const testValidations = [
@@ -23,6 +29,8 @@ const testValidations = [
     'type Ellipse = number',
     'type Ellipse = {}',
     'type Ellipse = []',
+
+    // object descriptions
     'type Ellipse = { cx }',
     'type Ellipse = { cx cy }',
     'type Ellipse = { cx { group { match cx { boolean } } } }',
@@ -33,11 +41,32 @@ const testValidations = [
     'type Ellipse = { cx { group { match cx { "first name" } } } }',
     'type Ellipse = { cx { group { match cx { string number } } } }',
     'type Ellipse = { cx { group { match cx { number } match cy { number } } } }',
+
     // initial with matching shortcuts. I use cy in match to make it clear that can be changed
     'type Ellipse = { cx { group { match cy { number } } } }',
     'type Ellipse = { cx { group { cy: number } } }',
     'type Ellipse = { cx { cy: number } }',
-    'type Ellipse = { cx: number }'
+    'type Ellipse = { cx: number }',
+
+    // array descriptions
+    'type Points = [ number ]',
+    'type Points = [ number, string ]',
+    'type Points = [ number;5..10 ]',
+    'type Points = [ number;5.. ]',
+    'type Points = [ number;..10 ]',
+    'type Points = [ number;5 ]',
+    'type Points = [ (number, string) ]',
+    'type Points = [ (number, string);5 ]',
+    'type Points = [ (number;5, string;3) ]',
+    'type Points = [ (number;5, string;3);10 ]',
+    'type Points = [ x: number ]',
+    'type Points = [ x: number, y: number ]',
+    'type Points = [ x: number;5 ]',
+    'type Points = [ pairs: (x: number; 5, y: string; 3); 10 ]',
+
+    // array as type
+    'type Points = { cx { points: [number, number] } }',
+    'type Points = { cx : x { } }'
 ];
 
 for (const source of testValidations) {
