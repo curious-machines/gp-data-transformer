f2af420474e4b387e28e1790046f102eb0a1cd3d
Kevin Lindsey <kevin@kevlindev.com>
2019-05-21 14:41:40 -0700

    Finalize polyline transform

--- scratchpad/affine.js
+++ scratchpad/affine.js
@@ -5,3 +5,16 @@ export function Point2D(x, y) {
 export function Vector2D(x, y) {
     return {x, y};
 }
+
+export function ListOfPoints(ps) {
+    const result = [];
+
+    for (let i = 0; i < ps.length; i += 2) {
+        const x = ps[i];
+        const y = ps[i + 1];
+
+        result.push(Point2D(x, y));
+    }
+
+    return result;
+}
