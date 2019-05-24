bfc0a435ada6a81c6a9d5c159280015560d34a51
Kevin Lindsey <kevin@kevlindev.com>
2019-05-23 17:35:02 -0700

    Add another spread test
    This looks more like something spread would be used for

--- test/spread-test.js
+++ test/spread-test.js
@@ -6,10 +6,17 @@ function Point2D(x, y) {
     return { x, y };
 }
 
+function sum(...nums) {
+    return nums.reduce((accum, num) => {
+        return accum + num;
+    }, 0);
+}
+
 function evaluate(source, structure) {
     const transformer = new Transformer();
 
     transformer.typeCreators.Point2D = Point2D;
+    transformer.typeCreators.sum = sum;
 
     const result = transformer.execute(source, structure);
 
@@ -41,6 +48,14 @@ describe("Spread", () => {
         const script = "map(coords, Point2D(...coord) <= array as coord) <= [ (number, number); 0.. as coords ]";
         const data = [1, 2, 3, 4, 5, 6];
         const expected = [Point2D(1, 2), Point2D(3, 4), Point2D(5, 6)];
+        const result = evaluate(script, data);
+
+        assert.deepStrictEqual(result, expected);
+    });
+    it("Sum", () => {
+        const script = "sum(...firsts, ...seconds) <= [ number; 1.. as firsts, string, number; 1.. as seconds ]";
+        const data = [1, 2, 3, "b", 4, 5, 6];
+        const expected = 21;
         const result = evaluate(script, data);
 
         assert.deepStrictEqual(result, expected);
