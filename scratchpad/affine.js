5e4d8b7c18f5825733262b8d3ea63e24fa199cc4
Kevin Lindsey <kevin@kevlindev.com>
2019-05-26 13:40:29 -0700

    Remove unneeded functions

--- scratchpad/affine.js
+++ scratchpad/affine.js
@@ -6,26 +6,7 @@ function Vector2D(x, y) {
     return {x, y};
 }
 
-function ListOfCoords(ps) {
-    const result = [];
-
-    for (let i = 0; i < ps.length; i += 2) {
-        const x = ps[i];
-        const y = ps[i + 1];
-
-        result.push(Point2D(x, y));
-    }
-
-    return result;
-}
-
-function ListOfPoints(ps) {
-    return ps.map(p => new Point2D(p.x, p.y));
-}
-
 module.exports = {
     Point2D: Point2D,
-    Vector2D: Vector2D,
-    ListOfCoords: ListOfCoords,
-    ListOfPoints: ListOfPoints
+    Vector2D: Vector2D
 }
