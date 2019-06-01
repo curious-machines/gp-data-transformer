2da22f977bf855dc1fea26041074039236c683d2
Kevin Lindsey <kevin@kevlindev.com>
2019-06-01 16:26:18 -0700

    Update dist

--- dist/index-umd.js
+++ dist/index-umd.js
@@ -4646,6 +4646,8 @@
   }();
 
   // import e from "estree-builder";
+
+  /* eslint-disable-next-line no-undef */
   var e = require("estree-builder");
 
   var $ = e.identifier("$");
@@ -4655,11 +4657,13 @@
    * Create a function that returns an empty object if a test is true
    *
    * @param {Object} testNode
+   * @param {string | null} assignTo
    * @returns {Object}
    */
 
-  function testFunction(testNode) {
-    var returnNode = e["return"](e.ternary(testNode, FAILURE_VALUE$1, emptyObject));
+  function testFunction(testNode, assignTo) {
+    var successValue = assignTo === null || assignTo === undefined ? emptyObject : e("object-raw", [e("object-property", e.string(assignTo), $)]);
+    var returnNode = e["return"](e.ternary(testNode, FAILURE_VALUE$1, successValue));
     return e["function"](["$"], [returnNode], "main");
   }
   /**
@@ -4667,34 +4671,37 @@
    *
    * @param {Object} left
    * @param {Object} right
+   * @param {string | null} assignTo
    * @returns {Object}
    */
 
 
-  function testInequalityFunction(left, right) {
-    return testFunction(e("!==", left, right));
+  function testInequalityFunction(left, right, assignTo) {
+    return testFunction(e("!==", left, right), assignTo);
   }
   /**
    * Create a function that tests its input value's type
    *
    * @param {string} typeName
+   * @param {string | null} assignTo
    * @returns {Object}
    */
 
 
-  function testType(typeName) {
-    return testInequalityFunction(e["typeof"]($), e.string(typeName));
+  function testType(typeName, assignTo) {
+    return testInequalityFunction(e["typeof"]($), e.string(typeName), assignTo);
   }
   /**
    * Create a function that tests that its input is a specific value
    *
    * @param {Object} valueNode
+   * @param {string | null} assignTo
    * @returns {*}
    */
 
 
-  function testValue(valueNode) {
-    return testInequalityFunction($, valueNode);
+  function testValue(valueNode, assignTo) {
+    return testInequalityFunction($, valueNode, assignTo);
   }
   /**
    * CodeGenerator class
@@ -4768,6 +4775,19 @@
             this.generatePattern(node);
             break;
 
+          case "array":
+          case "boolean":
+          case "null":
+          case "number":
+          case "object":
+          case "string":
+          case "undefined":
+            {
+              var returnValue = this.generateExpression(node);
+              this.append(e["function"]([$], [e["return"](returnValue)], "main"));
+              break;
+            }
+
           default:
             throw new TypeError("unknown node type: ".concat(node.type));
         }
@@ -4780,31 +4800,7 @@
         if (sequence.type === "sequence") {
           if (sequence.steps.length === 1) {
             var step = sequence.steps[0];
-
-            switch (step.type) {
-              case "boolean":
-                this.append(e["let"](assign.name, step.value ? e["true"]() : e["false"]()));
-                break;
-
-              case "null":
-                this.append(e["let"](assign.name, e["null"]()));
-                break;
-
-              case "number":
-                this.append(e["let"](assign.name, e.number(step.value)));
-                break;
-
-              case "string":
-                this.append(e["let"](assign.name, e.string(step.value)));
-                break;
-
-              case "undefined":
-                this.append(e["let"](assign.name, e.undefined()));
-                break;
-
-              default:
-                throw new TypeError("unsupported assignment value type: ".concat(step.type));
-            }
+            this.append(e["let"](assign.name, this.generateExpression(step)));
           }
         } else {
           throw new TypeError("Expected sequence as assignment value: ".concat(sequence.type));
@@ -4838,59 +4834,96 @@
         }
       }
     }, {
+      key: "generateExpression",
+      value: function generateExpression(expr) {
+        var _this = this;
+
+        switch (expr.type) {
+          case "array":
+            return e.array(expr.value.map(function (element) {
+              return _this.generateExpression(element);
+            }));
+
+          case "boolean":
+            return expr.value ? e["true"]() : e["false"]();
+
+          case "null":
+            return e["null"]();
+
+          case "number":
+            return e.number(expr.value);
+
+          case "object":
+            return e("object-raw", expr.value.map(function (property) {
+              return e("object-property", _this.generateExpression(property.name), // TODO: fix here and in assignment
+              _this.generateExpression(property.value.steps[0]));
+            }));
+
+          case "string":
+            return e.string(expr.value);
+
+          case "undefined":
+            return e.undefined();
+
+          default:
+            throw new TypeError("Unsupported expression type: ".concat(expr.type));
+        }
+      }
+    }, {
       key: "generatePattern",
       value: function generatePattern(pat) {
         switch (pat.patternType) {
           case "any":
             {
-              var returnNode = e["return"](e.object({}));
+              // return { "a": $ }
+              var returnNode = pat.assignTo === null ? e["return"](e.object({})) : e["return"](e("object-raw", [e("object-property", e.string(pat.assignTo), $)]));
               this.append(e["function"](["$"], [returnNode], "main"));
               break;
             }
 
           case "array":
-            this.append(testInequalityFunction(e.call(e(".", e.identifier("Array"), e.identifier("isArray")), [$]), e["true"]()));
+            this.append(testInequalityFunction(e.call(e(".", e.identifier("Array"), e.identifier("isArray")), [$]), e["true"](), pat.assignTo));
             break;
 
           case "boolean":
             if (pat.value === null) {
-              this.append(testType(pat.patternType));
+              this.append(testType(pat.patternType, pat.assignTo));
             } else if (pat.value === true) {
-              this.append(testValue(e["true"]()));
+              this.append(testValue(e["true"](), pat.assignTo));
             } else {
-              this.append(testValue(e["false"]()));
+              this.append(testValue(e["false"](), pat.assignTo));
             }
 
             break;
 
           case "number":
             if (pat.value === null) {
-              this.append(testType(pat.patternType));
+              this.append(testType(pat.patternType, pat.assignTo));
             } else {
-              this.append(testValue(e.number(pat.value)));
+              this.append(testValue(e.number(pat.value), pat.assignTo));
             }
 
             break;
 
           case "null":
-            this.append(testValue(e["null"]()));
+            this.append(testValue(e["null"](), pat.assignTo));
             break;
 
           case "object":
-            this.append(testInequalityFunction(e.call(e.identifier("isObject"), [$]), e["true"]()));
+            this.append(testInequalityFunction(e.call(e.identifier("isObject"), [$]), e["true"](), pat.assignTo));
             break;
 
           case "string":
             if (pat.value === null) {
-              this.append(testType(pat.patternType));
+              this.append(testType(pat.patternType, pat.assignTo));
             } else {
-              this.append(testValue(e.string(pat.value)));
+              this.append(testValue(e.string(pat.value), pat.assignTo));
             }
 
             break;
 
           case "undefined":
-            this.append(testValue(e.undefined()));
+            this.append(testValue(e.undefined(), pat.assignTo));
             break;
 
           default:
